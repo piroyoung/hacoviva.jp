643cfb73c58de1885a3970994b0e7ae9a06b58ea
hiroki <hiroki@users.noreply.github.com>
2021-02-28 20:41:44 +0900

    edit: ci

---
+++ src/render.py
@@ -0,0 +1,39 @@
+from typing import List
+
+from jinja2 import Environment
+from jinja2 import PackageLoader
+from jinja2 import select_autoescape
+
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Renderer:
+    environment: Environment
+
+    @classmethod
+    def of_default(cls):
+        return cls(
+            environment=Environment(
+                loader=PackageLoader("src"),
+                autoescape=select_autoescape(["html"])
+            )
+        )
+
+    def render(self, filename: str, *args, **kwargs) -> str:
+        return self.environment.get_template(filename).render()
+
+
+pages: List[str] = [
+    "index.html"
+]
+
+
+if __name__ == "__main__":
+    r = Renderer.of_default()
+    for p in pages:
+        with open(f"rendered/{p}", "w") as out:
+            out.write(r.render(p))
+
+
+
--- src/renderer.py
+++
@@ -1,4 +0,0 @@
-import jinja2
-
-if __name__ == "__main__":
-    pass
